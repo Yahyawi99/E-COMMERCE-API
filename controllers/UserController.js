e099477d99a2c9938e7923fa7c5465d58f9eba26
Yahyawi99 <yassinyahaywi26@gmail.com>
2023-09-04 19:23:37 +0100

    working on user routes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,23 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
 const getAllUsers = async (req, res) => {
-  res.send("send all users");
+  const users = await User.find({ role: "user" }).select("-password");
+
+  res.status(StatusCodes.OK).json({ users });
 };
 
 const getSingleUser = async (req, res) => {
-  res.send("send single user");
+  const { id } = req.params;
+
+  const user = await User.findOne({ _id: id }).select("-password");
+
+  if (!user) {
+    throw new CustomError.NotFoundError(`Nouser with id : ${id}`);
+  }
+
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
