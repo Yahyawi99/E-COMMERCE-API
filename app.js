548d357b8af12c952845037c1b3b5749f78cb1c5
Yahyawi99 <yassinyahaywi26@gmail.com>
2023-09-11 18:41:31 +0100

    security packages

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const fileUpload = require("express-fileupload");
 // rest of the packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 
 // database
 const connectDB = require("./db/connect");
@@ -27,6 +32,14 @@ const OrderRouter = require("./routes/orderRoute");
 // Middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// secyrity packages
+app.set("trust proxy", 1);
+app.use(rateLimiter({ windowsMs: 15 * 60 * 1000, max: 60 }));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(express.json());
 app.use(morgan("tiny"));
